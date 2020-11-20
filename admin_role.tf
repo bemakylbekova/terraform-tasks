70edd00316c3072cd4ee0a31e3da3f949fec3fe7
bemakylbekova <bemaangel49@gmail.com>
2020-11-20 02:01:06 +0000

    task2b

---
+++ admin_role.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "Admin_role" {
+name = "admin"
+assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+{
+"Action": "sts:AssumeRole",
+"Principal": {
+"Service": "ec2.amazonaws.com"
+},
+"Effect": "Allow",
+"Sid": ""
+}
+]
+}
+EOF
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "AdminAccess"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
--- instance.tf
+++ instance.tf
@@ -1,15 +1,6 @@
-resource "aws_instance" "Ubuntu" {
-	ami = "ami-0739f8cdb239fe9ae"
+resource "aws_instance" "Centos6" {
+	ami = "ami-e3fdd999"
 	instance_type = "t2.micro"
     associate_public_ip_address = "true"
-    key_name = "${aws_key_pair.Bastion.key_name}"
-    user_data = "${file("userdata_file.sh")}"
-    vpc_security_group_ids = ["${aws_security_group.task2.id}"]
-    availability_zone = "us-east-1a"
-    
-    
-    tags = {
-      Name        = "Ubuntu"
-      Environment = "Dev"
-    }
+    key_name = "${aws_key_pair.Centos6.key_name}"
 }
